7a60a2eefbaf20fb23d521259a829405e78f924d
hamisicodes <newhamisi@gmail.com>
2020-04-24 19:53:09 +0300

    create the Source class and its constructor

--- app/test_source.py
+++ app/test_source.py
@@ -9,9 +9,13 @@ class testSource(unittest.TestCase):
         self.new_source  = Source("ke-news" , "Kenya News" , "Home of the latest kenyan news", "www.kenyanews.com")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_source),Source)
+        self.assertTrue(isinstance(self.new_source,Source))
 
-
+    def test_init(self):
+        self.assertEqual(self.new_source.id,"ke-news")
+        self.assertEqual(self.new_source.name,"Kenya News")
+        self.assertEqual(self.new_source.description,"Home of the latest kenyan news")
+        self.assertEqual(self.new_source.url,"www.kenyanews.com")
 
 
 if __name__ == '__main__':
