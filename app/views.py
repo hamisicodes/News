614703178ba8bce8e93710e9b92ae2063ee00477
hamisicodes <newhamisi@gmail.com>
2020-04-24 21:31:19 +0300

    perfomthe first API call passing sports as the category

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template
-
+from requests import get_sources
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sports_sources = get_sources('sports')
+    return render_template('index.html' , sports_sources = sports_sources)
