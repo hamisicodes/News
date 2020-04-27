8d84088f046ce09d84d94f48bcaf031c1036693e
hamisicodes <newhamisi@gmail.com>
2020-04-27 06:49:42 +0300

    update __init__.py by creating a factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
-
-
-app = Flask(__name__,instance_relative_config=True)
-
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+from config import config_options
 
 bootstrap = Bootstrap(app)
+
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+    return app
+
+
 
 
 
