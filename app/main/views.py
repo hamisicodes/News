570cf05bd002f47b1dfe2b252032b32444cf9600
hamisicodes <newhamisi@gmail.com>
2020-04-27 07:17:18 +0300

    update the views module with imports and the required route naming convention

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from app import app
 from flask import render_template
-from .requests import get_sources,get_articles
+from . import main
+from ..requests import get_sources,get_articles
 
-@app.route('/')
+@main.route('/')
 def index():
     general_sources = get_sources('general')
     sports_sources = get_sources('sports')
@@ -13,7 +13,7 @@ def index():
     return render_template('index.html' , sports_sources = sports_sources , general_sources = general_sources , science_sources = science_sources , business_sources = business_sources, technology_sources = technology_sources)
 
 
-@app.route('/source/<id>')
+@main.route('/source/<id>')
 def sources(id):
     articles = get_articles(id)
     return render_template('articles.html' , name = id , articles = articles)
--- app/requests.py
+++ app/requests.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request,json
-from .models import Source,Article
+from ..models import Source,Article
 
 
 
