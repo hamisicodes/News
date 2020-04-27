375c94502674e8c5e14dd1fbccbff1582b6792cb
hamisicodes <newhamisi@gmail.com>
2020-04-27 06:56:14 +0300

    create blueprint and register it

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,10 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
 
 
